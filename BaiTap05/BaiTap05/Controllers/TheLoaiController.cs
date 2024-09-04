6d89e99f049dd9fb9476e0acc5fcb8cbd2bcee64
manhsuke <anhnguyen11203@gmail.com>
2024-09-04 14:57:49 +0700

    Hoan Thanh bai tap 05 tren lop

---
+++ BaiTap05/BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 28";
+            ViewBag.Thang = "Tháng 9";
+            ViewData["Nam"] = "2024";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh Thám"
+            };
+            return View(theloai);
+
+        }
+    }
+}
