9f0147c5d01b7519aed75a015c9aea2d881d30df
Nguyen Hoang Manh <89469985+manhsuke@users.noreply.github.com>
2024-09-11 13:51:02 +0700

    Delete BaiTapThucHanh_4_9 directory

--- BaiTapThucHanh_4_9/BaiTapThucHanh_4_9/BaiTapThucHanh_4_9/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BaiTapThucHanh_4_9.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
