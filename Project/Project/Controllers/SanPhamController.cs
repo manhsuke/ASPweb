013fb35581fc716f5825c193834c8a6766347c31
manhsuke <anhnguyen11203@gmail.com>
2024-09-25 20:20:14 +0700

    lab 30

---
+++ Project/Project/Controllers/SanPhamController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Data;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    [Area("Admin")]
+    public class SanPhamController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SanPhamController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<SanPham> sanpham=_db.SanPham.Include("TheLoai").ToList();
+            return View(sanpham);
+        }
+    }
+}
---
+++ Project/Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+namespace Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TheLoai> TheLoai { get; set; }
+        public DbSet<SanPham> SanPham { get; set; }
+    }
+}
