69e62fc28c0dd454810773a3cf6aed938c2947a9
manhsuke <anhnguyen11203@gmail.com>
2024-09-28 15:21:49 +0700

    BaiKiemTra02

---
+++ BaiKiemTra02/BaiKiemTra02/Controllers/LopHocController.cs
@@ -0,0 +1,99 @@
+﻿using BaiKiemTra02.Data;
+using BaiKiemTra02.Data.Migrations;
+using BaiKiemTra02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra02.Controllers
+{
+    public class LopHocController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public LopHocController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var lophoc = _db.LopHoc.ToList();
+            ViewBag.lophoc = lophoc;
+            return View(lophoc);
+    
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Models.LopHoc lophoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.LopHoc.Add(lophoc);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.LopHoc.Find(id);
+
+            return View(theloai);
+        }
+        [HttpPost]
+        public IActionResult Edit(Models.LopHoc lophoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.LopHoc.Update(lophoc);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.LopHoc.Find(id);
+
+            return View(theloai);
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var theloai = _db.LopHoc.Find(id);
+
+            return View(theloai);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var theloai = _db.LopHoc.Find(id);
+            if (theloai == null)
+            {
+                return NotFound();
+            }
+            _db.LopHoc.Remove(theloai);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
